e91be8877b26787e606ff16c1fd1fa9e5f993eb5
Fabio Almeida <tugapse@gmail.com>
2026-05-13 19:43:58 +0100

    fix typo

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bashc
+#!/usr/bin/env bash
 SCRIPT_DIR="$( cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" >/dev/null 2>&1 && pwd )"
 
 "$SCRIPT_DIR/target/release/rs-temp" "$@"
