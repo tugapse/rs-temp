607811bb6c2514a7fb68915f08381c144d94a3c5
Fabio Almeida <tugapse@gmail.com>
2026-05-13 19:42:20 +0100

    build(build.sh): Add shebang and release build command
    - Add shebang line to build.sh
    - Run cargo build --release in build.sh
    - Add shebang line to run.sh
    - Compute SCRIPT_DIR as the directory of the current script
    - Execute the rs-temp binary from SCRIPT_DIR/target/release using "$@"

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-./target/release/rs-temp "$@"
+#!/usr/bin/env bashc
+SCRIPT_DIR="$( cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" >/dev/null 2>&1 && pwd )"
+
+"$SCRIPT_DIR/target/release/rs-temp" "$@"
